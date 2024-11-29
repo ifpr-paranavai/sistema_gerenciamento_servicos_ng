39dfaad888414ac0313612177f4ac2f0a57c426a
Lucas Rachid Martins <lucas.rachid@ymunity.com.br>
2024-11-29 16:11:54 -0300

    Messages

--- src/app/modules/message/message.component.ts
+++ src/app/modules/message/message.component.ts
@@ -1,22 +1,32 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 interface Contact {
-  id: number;
-  name: string;
-  avatar?: string | null;
+    id: number;
+    name: string;
+    avatar?: string | null;
 }
 
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'sgs-message',
+    templateUrl: './message.component.html',
+    styleUrls: ['./message.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessageComponent implements OnInit {
+    userMessages: string[] = ['Olá, tudo bem?'];
+    myMessages: string[] = ['Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?'];
+    newMessage: string = '';
 
-  constructor() {}
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    sendMessage() {
+        if (this.newMessage.trim()) {
+            this.myMessages.push(this.newMessage.trim());
+            this.newMessage = '';
+        }
+    }
 
 }
